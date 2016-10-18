18f859e75bd3aa8fecd085ab05330ddf279b48d9
Dan Pomohaci <dan.pomohaci@gmail.com>
2016-10-18 14:15:21 +0300

    adds log folder and system dependencies

--- project.clj
+++ project.clj
@@ -3,27 +3,33 @@
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
- :dependencies [[org.clojure/clojure "1.7.0"],
-                 [org.clojure/tools.nrepl "0.2.12"]
+
+  :dependencies [[org.clojure/clojure "1.7.0"],
                  [yetibot.core "0.3.16"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [org.danielsz/system "0.3.1"]
 
-                 ; logging
+                 ;; logging
                  [devth/timbre "3.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
 
-                 ; http
+                 ;; http
                  [clj-http "0.7.7"]
- 
-                 ; web
+
+                 ;; web
                  [compojure "1.4.0"]
 
-                 ; utils
+                 ;; utils
                  [clj-stacktrace "0.2.8"]
                  [useful "0.8.3-alpha8"]
                  [org.clojure/tools.cli "0.3.1"]
                  [environ "1.0.2"]
 
-                 ; NLP
+                 ;; NLP
                  [clojure-opennlp "0.3.2"]
 
                  ]
@@ -34,20 +40,34 @@
             [io.sarnowski/lein-docker "1.1.0"]]
 
   :aliases {"version" ["exec" "-ep" "(use 'pea.version)(print version)"]}
-  
+
   :pedantic :ignore
 
   :profiles {:check {:global-vars {*warn-on-reflection* true}}
              :dev {:source-paths ["dev"]
+                   :test-paths ["src"]
+                   :resource-dirs ["log"]
+                   :jvm-opts ["-Dlog4j.configuration=file:log/log4j.properties"]
+                   :env {:config-file "dev/dev.edn"}
+                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                  [org.clojure/tools.reader "0.9.2"]
+                                  [org.clojure/java.classpath "0.2.2"]
+                                  [org.clojure/tools.trace "0.7.8"]
+                                  [ring/ring-devel "1.3.2"]
+                                  [alembic "0.3.2"]
+                                  [org.clojure/tools.nrepl "0.2.12"]
+                                  [pjstadig/humane-test-output "0.7.0"]]
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]
                    }
-             :test {}
+             :production {}
              :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]}
 
   :repl-options {:init-ns pea.repl
                  :welcome (println "Welcome to the pea development REPL!")}
 
   :jvm-opts ["-server" "-Xmx2G"]
- 
+
   :docker {:image-name "dpom/pea"}
 
   :ring {:handler pea.handler/app
@@ -56,4 +76,4 @@
          :uberwar-name "pea.war"}
 
   :main pea.init
-)
+  )
--- src/pea/plugins/commands/howto.clj
+++ src/pea/plugins/commands/howto.clj
@@ -8,7 +8,7 @@
   "howto <topic> <key> # search in <topic> howtos <key> and display these howtos"
   {:yb/cat #{:util}}
   [{:keys [args]}]
-  (let [[topic key] (split args)]
+  (let [[topic key] (split args #" ")]
     (format "topic = %s, key = %s." topic key)))
 
 (cmd-hook #"howto"
